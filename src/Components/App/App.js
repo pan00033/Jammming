b847653c97e51ea7f8b81705341b608f77fb5fc1
ArizonaPeachTea <zuxingp888@gmail.com>
2021-02-21 18:40:54 -0600

    pass down playlist to tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {searchResults: [{name:'reputation',artist:'reputation',album:'reputation',id:'1'},
+    {name:'reputation',artist:'reputation',album:'reputation',id:'1'},
+    {name:'reputation',artist:'reputation',album:'reputation',id:'1'},
+    {name:'reputation',artist:'reputation',album:'reputation',id:'1'}],
+    playlistName:'Changes',playlistTracks:[{name:'intention',artist:'Justin',album:'Changes',id:'1'},
+    {name:'intention',artist:'Justin',album:'Changes',id:'1'}]};
+  }
+
+  
+
   render(){
     return(
       <div>
@@ -13,8 +24,8 @@ class App extends React.Component{
           <div className="App">
             <SearchBar />
             <div className="App-playlist">
-              <SearchResults />
-              <Playlist />
+              <SearchResults searchResults={this.state.searchResults}/>
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
             </div>
         </div>
       </div>
